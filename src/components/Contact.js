9d2a78894453998ce6e528080faafedaff097587
unknown <youngscientist.iysa@gmail.com>
2025-09-15 14:20:18 +0700

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,35 @@
+import { useEffect } from "react";
 const Contact = () => {
+  useEffect(() => {
+      const scriptURL =
+      "https://script.google.com/macros/s/AKfycbzdgUOy_s6zjJQTgqXQ7GX3H1_w6TvWq1hsBZgH0mSREWt3qXCKA34-qo74-jfDVbHE/exec";
+
+      const form = document.forms.namedItem("contact");
+
+      if (form) {
+      const handleSubmit = async (e) => {
+          e.preventDefault();
+          try {
+          await fetch(scriptURL, {
+              method: "POST",
+              body: new FormData(form),
+          });
+          alert("Message sent successfully!");
+          form.reset();
+          } catch (error) {
+          console.error("Error:", error);
+          alert("Failed to send Message.");
+          }
+      };
+
+      form.addEventListener("submit", handleSubmit);
+
+      // cleanup listener
+      return () => {
+          form.removeEventListener("submit", handleSubmit);
+      };
+      }
+  }, []);
   return (
     <div id="conts" class="contact">
       <div class="container">
@@ -6,23 +37,16 @@ const Contact = () => {
           <div class="box">
             <h1>Kontak</h1>
             <div class="form-contact">
-              <form action="https://formspree.io/f/mvoeebej" method="POST">
+              <form action="" method="POST" name="contact">
                 <table>
+                  <input type="hidden" name="Event" value="NRTC" readOnly />
                   <tr>
                     <td>
                       <input
                         type="text"
-                        name="Nama Depan"
-                        placeholder="Nama Depan"
+                        name="Name"
+                        placeholder="Nama"
                         required
-                        autocomplete="off"
-                      />
-                    </td>
-                    <td>
-                      <input
-                        type="text"
-                        name="Nama Belakang"
-                        placeholder="Nama Belakang"
                         autocomplete="off"
                       />
                     </td>
@@ -31,7 +55,7 @@ const Contact = () => {
                     <td colspan="2">
                       <input
                         type="email"
-                        name="email"
+                        name="Email"
                         placeholder="Email"
                         required
                         autocomplete="off"
@@ -41,7 +65,7 @@ const Contact = () => {
                   <tr>
                     <td colspan="2">
                       <textarea
-                        name="text"
+                        name="Message"
                         cols="30"
                         rows="10"
                         placeholder="Pesan"
