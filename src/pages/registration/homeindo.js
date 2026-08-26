657a1e9d66ade1eb3d65c6bab456434cbc9d2550
iysa <youngscientist.iysa@gmail.com>
2026-08-26 21:22:43 +0700

    fix

--- src/pages/registration/homeindo.js
+++ src/pages/registration/homeindo.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import { indonesiaOnlineTerms, indonesiaOfflineTerms } from "../data/term";
 import Navbar from "../../components/nrtc/Navbar";
